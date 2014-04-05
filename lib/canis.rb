0712d675a1e54fda9350ef33fb46f4e8feb6cd22
kepler <githubkepler.50s@gishpuppy.com>
2014-04-05 13:57:00 +0530

    minor updates

--- lib/canis.rb
+++ lib/canis.rb
@@ -1,4 +1,9 @@
 require "canis/version"
+require 'ffi-ncurses' 
+require 'canis/core/system/ncurses'
+require 'canis/core/system/window'
+require 'canis/core/widgets/rwidget'
+require 'canis/core/util/rdialogs'
 
 module Canis
   # Your code goes here...
