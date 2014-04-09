490762f9fc54aaf247581ee0f799913da4253820
kepler <githubkepler.50s@gishpuppy.com>
2014-04-09 16:02:43 +0530

    adding textpad to preload

--- lib/canis.rb
+++ lib/canis.rb
@@ -3,6 +3,8 @@ require 'ffi-ncurses'
 require 'canis/core/system/ncurses'
 require 'canis/core/system/window'
 require 'canis/core/widgets/rwidget'
+# added textpad here since it is now the basis of so many others 2014-04-09 - 12:57 
+require 'canis/core/widgets/textpad'
 require 'canis/core/util/rdialogs'
 
 module Canis
