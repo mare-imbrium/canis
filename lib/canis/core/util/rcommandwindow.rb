a4c6a9fb798759819383232dee578c87af0a0a6a
kepler <githubkepler.50s@gishpuppy.com>
2014-04-26 11:03:38 +0530

    keeping only essential stuff

--- lib/canis/core/util/rcommandwindow.rb
+++ lib/canis/core/util/rcommandwindow.rb
@@ -2,19 +2,22 @@
   * Name: rcommandwindow: pops up a status message at bottom of screen
           creating a new window, so we don't have to worry about having window
           handle.
-          Can use with say, ask etc. wsay wask wagree etc !
+
   * Description   
   * Author: jkepler (ABCD)
   * Date: 2008-11-19 12:49 
   * License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
   * file separated on 2009-01-13 22:39 
 
+  == Changes
+ Have removed a lot of stuff that was either a duplication of other stuff, or that was not really
+ adding any value. 
+ 
 == Issues
-   Depends on bottomline for some operations such as ask, agree etc. 
-   Pls remove this dependence
 
-   Creates a ListObject which duplicates textview and listboxes work
-   This should be replaced with textpad
+   Only display_menu is of any use, and can be taken out and placed in some util or extra.
+   It is called by numbered_menu which can be retained.
+   display_menu is used by Promptmenu too.
 
 =end
 require 'canis'
@@ -41,7 +44,7 @@ module Canis
       @config.each_pair { |k,v| instance_variable_set("@#{k}",v) }
       instance_eval &block if block_given?
       if @layout.nil? 
-          set_layout(1,80, -1, 0) 
+          set_layout(1,Ncurses.COLS, -1, 0) 
       end
       @height = @layout[:height]
       @width = @layout[:width]
@@ -69,34 +72,22 @@ module Canis
       end
     end  # --- }}}
 
-    #--- bottomline related {{{
-    def make_bottomline
-      unless @bottomline
-        require 'canis/core/util/extras/bottomline'
-        @bottomline = Bottomline.new @window, 0
-        @bottomline.name = "rcommandwindow's bl"
-      end
-      return @bottomline
+    # not sure if this is really required. print_string is just fine.
+    # print a string.
+    # config can be :x :y :color_pair
+    def print_str text, config={}
+      win = config.fetch(:window, @window) # assuming its in App
+      x = config.fetch :x, 0 
+      y = config.fetch :y, 0
+      color = config[:color_pair] || $datacolor
+      raise "no window for ask print in #{self.class} name: #{name} " unless win
+      color=Ncurses.COLOR_PAIR(color);
+      win.attron(color);
+      win.mvprintw(x, y, "%s" % text);
+      win.attroff(color);
+      win.refresh # FFI NW 2011-09-9  , added back gets overwritten
     end
-    def ask *args
-      make_bottomline
-      @bottomline.send(:ask, *args)
-    end
-    def say *args
-      make_bottomline
-      @bottomline.send(:say, *args)
-    end
-    def agree *args
-      make_bottomline
-      @bottomline.send(:agree, *args)
-    end
-    # NOTE the ridiculous thing is that choose makes a rcommandwindow and then
-    # calls display_menu on rc ! it does not really seem to use bottomline at all.
-    def choose *args
-      make_bottomline
-      @bottomline.send(:choose, *args)
-    end
-    #--- bottomline related }}}
+
 
 
     # ---- windowing functions {{{
@@ -256,7 +247,7 @@ module Canis
           end
           @window.printstring i+@row_offset, 1, text, $normalcolor  
           if indexing
-            window.mvchgat(y=i+@row_offset, x=1, max=1, indexatt, indexcolor, nil)
+            @window.mvchgat(y=i+@row_offset, x=1, max=1, indexatt, indexcolor, nil)
           end
         }
       else
@@ -314,324 +305,98 @@ module Canis
       @window.wrefresh
     end # --- }}}
 
-    # displays given text and handles keys
-    def display_interactive text, config={}  # --- {{{
-      if @to
-        @to.content text
-      else
-        config[:box] = @box
-        @to = ListObject.new self, text, config
-      end
-      yield @to if block_given?
-      @to.display_interactive # this returns the item selected
-      @to   # this will return the ListObject to the user with list and current_index
-    end  # --- }}}
-
-    # non interactive list display 
-    def udisplay_list text, config={}  # --- {{{
-      if @to
-        @to.content text
-      else
-        config[:box] = @box
-        @to = ListObject.new self, text, config
-      end
-      yield @to if block_given?
-      @to.display_content
-      @to
-    end  # --- }}}
-
-    # --- ListObject {{{
-    # displays a list
-    # Why did we create another list ? 2011-12-5 
-    class ListObject
-      attr_reader :cw
-      attr_reader :list
-      attr_reader :current_index
-      attr_accessor :focussed_attrib
-      attr_accessor :focussed_symbol
-      def initialize cw, _list, config={}
-        @cw  = cw
-        layout = @cw.layout
-        @window = @cw.window
-        @height = layout[:height]
-        @width = layout[:width]
-        content(_list)
-        @height_adjust = config.fetch(:box, true) == :border ? 3 : 2
-        @selected_index = nil
-        @current_index = 0
-        @row_offset = 1
-        @toprow = 0
-        $multiplier = 0 # till we can do something
-
-        @focussed_symbol = ''
-        @row_selected_symbol = ''
-        #@show_selector = true
-        if @show_selector
-          @row_selected_symbol ||= '*'
-          @row_unselected_symbol ||= ' '
-          @left_margin ||= @row_selected_symbol.length
-        end
-        #@show_selector = true
-        #@row_selected_symbol = '*'
-        #@row_unselected_symbol = ' '
-      end
-      def content txt, config={}
-        @current_index = 0 # sometimes it gets left at a higher value than there are rows to show
-        case txt
-        when String
-          txt = wrap_text(txt, @width-2).split("\n")
-        when Array
-          # okay
-        end
-        @list = txt
-      end
-      # maybe we should just use a textview or label rather than try to 
-      # do it all voer again !
-      def display_interactive
-        display_content
-        while !@stop
-          @window.wrefresh # FFI 2011-09-12 
-          # FIXME only clear and redisplay if change has happened (repaint_require)
-          handle_keys { |ch| @cw.clear; display_content }
-        end
-        return @list[@current_index]
-      end
-      def is_row_selected row
-        @selected_index == row
-      end
-
-      def scrollatrow
-        @height - 3
-      end
-      def row_count
-        @list.size
-      end
-      def rowcol
-        return @row_offset, @col_offset
-      end
-      def display_content #:nodoc:
-
-        @graphic = @window
-        @start ||= 0
-        @toprow ||= 0
-        @left_margin ||= @row_selected_symbol.length + @focussed_symbol.length
-
-        #print_borders unless @suppress_borders # do this once only, unless everything changes
-        #maxlen = @maxlen ||= @width-2
-        tm = @list
-        rc = tm.size
-        @col_offset = 1
-        #@longest_line = @width
-        if rc > 0     # just added in case no data passed
-          #tr = @start #@toprow
-          tr = @toprow
-          acolor = get_color $datacolor
-          #h = @height - 3 #2
-          h = @height - @height_adjust
-          r,c = @row_offset, @col_offset
-          0.upto(h) do |hh|
-            crow = tr+hh
-            if crow < rc
-              focussed = @current_index == crow  # row focussed ?
-              selected = is_row_selected crow
-              content = tm[crow]   # 2009-01-17 18:37 chomp giving error in some cases says frozen
-              # by now it has to be a String
-              ## set the selector symbol if requested
-              selection_symbol = ''
-              if @show_selector
-                if selected
-                  selection_symbol = @row_selected_symbol
-                else
-                  selection_symbol =  @row_unselected_symbol
-                end
-                @graphic.printstring r+hh, c, selection_symbol, acolor,@attr
-              end
-              #renderer = get_default_cell_renderer_for_class content.class.to_s
-              if focussed
-                if @focussed_symbol
-                  @graphic.printstring r+hh, c, @focussed_symbol, acolor,@attr
-                end
-                @graphic.printstring r+hh, c+@left_margin, content, acolor, @focussed_attrib || 'reverse'
-              else
-                @graphic.printstring r+hh, c+@left_margin, content, acolor,@attr
-              end
-              #renderer.repaint @graphic, r+hh, c+@left_margin, crow, content, focussed, selected
-            else
-              # clear rows
-              @graphic.printstring r+hh, c, " " * (@width-2), acolor,@attr
-            end
-          end
-        end # rc == 0
-        set_form_row
-      end
-      # listobject
-      def press ch # list TODO copy from rbasiclist
-        ch = ch.getbyte(0) if ch.class==String ## 1.9
-        $log.debug " XXX press #{ch} " if $log.debug? 
-        case ch
-        when -1
-          return
-        when KEY_F1, 27, ?\C-q.getbyte(0)   
-          @stop = true
-          return
-        when KEY_ENTER, 10, 13
-          #$log.debug "popup ENTER : #{@selected_index} "
-          #$log.debug "popup ENTER :  #{field.name}" if !field.nil?
-          @stop = true
-          return
-        when 32, ?\C-d.getbyte(0)
-          scroll_forward
-          #@start += @height-1
-          #bounds_check
-        when KEY_UP, ?k.getbyte(0)
-          previous_row
-          #@start -= 1
-          #@current_index -= 1
-          #@current_index = 0 if @current_index < 0
-          #@start = 0 if @start < 0
-        when KEY_DOWN, ?j.getbyte(0)
-          next_row
-          #@start += 1
-          #@current_index += 1
-          #bounds_check
-        when ?\C-b.getbyte(0)
-          scroll_backward
-          #@start -= @height-1
-          #@start = 0 if @start < 0
-        when 0, ?g.getbyte(0)
-          goto_top
-        when ?G.getbyte(0)
-          goto_bottom
-        end
-        #@form.repaint
-        Ncurses::Panel.update_panels();
-        Ncurses.doupdate();
-        @window.wrefresh
-      end
-
-      def previous_row num=(($multiplier.nil? or $multiplier == 0) ? 1 : $multiplier)
-        #return :UNHANDLED if @current_index == 0 # EVIL
-        return false if @current_index == 0 
-        @oldrow = @current_index
-        num.times { 
-          @current_index -= 1 if @current_index > 0
-        }
-        bounds_check
-        $multiplier = 0
-      end
-      alias :up :previous_row
-      def next_row num=(($multiplier.nil? or $multiplier == 0) ? 1 : $multiplier)
-        rc = row_count
-        return false if @current_index == rc-1 
-        @oldrow = @current_index
-        @current_index += 1*num if @current_index < rc
-        bounds_check
-        $multiplier = 0
-      end
-      alias :down :next_row
-      def goto_bottom
-        @oldrow = @current_index
-        rc = row_count
-        @current_index = rc -1
-        bounds_check
-      end
-      alias :goto_end :goto_bottom
-      def goto_top
-        @oldrow = @current_index
-        @current_index = 0
-        bounds_check
-      end
-      alias :goto_start :goto_top
-      def scroll_backward
-        @oldrow = @current_index
-        h = scrollatrow()
-        m = $multiplier == 0? 1 : $multiplier
-        @current_index -= h * m
-        bounds_check
-        $multiplier = 0
-      end
-      def scroll_forward
-        @oldrow = @current_index
-        h = scrollatrow()
-        rc = row_count
-        m = $multiplier == 0? 1 : $multiplier
-        # more rows than box
-        if h * m < rc
-          @toprow += h+1 #if @current_index+h < rc
-          @current_index = @toprow
-        else
-          # fewer rows than box
-          @current_index = rc -1
-        end
-        #@current_index += h+1 #if @current_index+h < rc
-        bounds_check
-      end
-
-      ##
-      # please set oldrow before calling this. Store current_index as oldrow before changing. NOTE
-      def bounds_check
-        h = scrollatrow()
-        rc = row_count
-
-        @current_index = 0 if @current_index < 0  # not lt 0
-        @current_index = rc-1 if @current_index >= rc && rc>0 # not gt rowcount
-        @toprow = rc-h-1 if rc > h && @toprow > rc - h - 1 # toprow shows full page if possible
-        # curr has gone below table,  move toprow forward
-        if @current_index - @toprow > h
-          @toprow = @current_index - h
-        elsif @current_index < @toprow
-          # curr has gone above table,  move toprow up
-          @toprow = @current_index
-        end
-        set_form_row
-
-      end
-
-      def set_form_row
-        r,c = rowcol
-
-
-        # when the toprow is set externally then cursor can be mispositioned since 
-        # bounds_check has not been called
-        if @current_index < @toprow
-          # cursor is outside table
-          @current_index = @toprow # ??? only if toprow 2010-10-19 12:56 
-        end
-
-        row = r + (@current_index-@toprow) 
-        # row should not be < r or greater than r+height TODO FIXME
-
-        #setrowcol row, nil
-        @window.wmove row, c
-        @window.wrefresh   # FFI added to keep cursor display in synch with selection
-      end
-      def OLDbounds_check #:nodoc:
-        @start = 0 if @start < 0
-        row_offset = 1
-        last = (@list.length)-(@height-row_offset-1)
-        if @start > last
-          @start = last
-        end
-      end # bounds_check
-      def handle_keys #:nodoc:
-        begin
-          while((ch = @window.getchar()) != 999 )
-            case ch
-            when -1
-              next
-            else
-              press ch
-              break if @stop
-              yield ch if block_given?
-            end
-          end
-        ensure
-          @cw.destroy  
-        end
-        return @current_index
-      end
-    end # class ListObject
-    # --- ListObject }}}
 
   end # class CommandWindow
+
+  # presents given list in numbered format in a window above last line
+  # and accepts input on last line
+  # The list is a list of strings. e.g.
+  #      %w{ ruby perl python haskell }
+  # Multiple levels can be given as:
+  #      list = %w{ ruby perl python haskell }
+  #      list[0] = %w{ ruby ruby1.9 ruby 1.8 rubinius jruby }
+  # In this case, "ruby" is the first level option. The others are used
+  # in the second level. This might make it clearer. first3 has 2 choices under it.
+  #      [ "first1" , "first2", ["first3", "second1", "second2"], "first4"]
+  #
+  # Currently, we return an array containing each selected level
+  #
+  # @return [Array] selected option/s from list
+  def numbered_menu list1, config={}
+    if list1.nil? || list1.empty?
+      #say_with_pause "empty list passed to numbered_menu"  # 2014-04-25
+       # remove bottomline
+      print_error_message "Empty list passed to numbered_menu"
+      return nil
+    end
+    prompt = config[:prompt] || "Select one: "
+    require 'canis/core/util/rcommandwindow'
+    layout = { :height => 5, :width => Ncurses.COLS-1, :top => Ncurses.LINES-6, :left => 0 }
+    rc = CommandWindow.new nil, :layout => layout, :box => true, :title => config[:title]
+    w = rc.window
+    # should we yield rc, so user can bind keys or whatever
+    # attempt a loop so we do levels.
+    retval = []
+    begin
+      while true
+        rc.display_menu list1, :indexing => :number
+        #ret = ask(prompt, Integer ) { |q| q.in = 1..list1.size }
+        # FIXME valid range won't work here since on_leave is not
+        # triggered.
+        ret = rb_gets(prompt) {|f| f.datatype = 1.class ; f.type :integer; f.valid_range(1..list1.size)}
+        val = list1[ret-1]
+        if val.is_a? Array
+          retval << val[0]
+          $log.debug "NL: #{retval} "
+          list1 = val[1..-1]
+          rc.clear
+        else
+          retval << val
+          $log.debug "NL1: #{retval} "
+          break
+        end
+      end
+    ensure
+      rc.destroy
+      rc = nil
+    end
+    #list1[ret-1]
+    $log.debug "NL2: #{retval} , #{retval.class} "
+    retval
+  end
+  # NOTE:  moved from bottomline since it used commandwindow but now we;ve
+  # moved away from ListObject to view. and i wonder what this really
+  # gives ?
+  # WARNING: if you actually use this, please copy it to your app, since
+  # it may be removed. I don;t see what real purpose it achieves. It is
+  # now a wrapper over Canis::Viewer.view
+  # 
+  # Displays text at the bottom of the screen, close the screen with
+  # ENTER. 
+  # @param text can be file name or Array of Strings.
+  # @param config is a Hash. :height which will be from bottom of screen
+  # and defaults to 15.
+  # All other config elements are passed to +view+. See viewer.rb.
+  def display_text text, config={}
+    ht = config[:height] || 15
+    sh = Ncurses.LINES-1
+    sc = Ncurses.COLS-0
+    layout = { :height => ht, :width => Ncurses.COLS-1, :top => Ncurses.LINES-ht+1, :left => 0 }
+    layout = [ 10,0,20,80]
+    layout = [ sh-ht ,0,ht,sc]
+    config[:layout] = layout
+    config[:close_key] = KEY_ENTER
+    $log.debug "XXX:  DISP TEXT #{config[:title]} "
+    view(text, config) do |t|
+      t.suppress_borders true
+    end
+  end
+  # return a blank if user quits list, or the value
+  # can we prevent a user from quitting, he must select ? 
+  # Can we suppress borders but have a reverse bar above, like the old
+  # one ?
+  def display_list text, config={}
+    $log.debug "XXX:  DISP #{config[:title]} "
+    ret = display_text text, config
+  end
 end # module
