5532e858863f02b2b3ecf2cb0fc211680dc8754e
kepler <githubkepler.50s@gishpuppy.com>
2014-04-30 01:20:32 +0530

    touch up

--- examples/bline.rb
+++ examples/bline.rb
@@ -14,9 +14,9 @@ def testnumberedmenu
   message "We got #{str} "
 end
 def testdisplay_list
-  # scrollable list
+  # scrollable filterable list
   text = Dir.glob "*.rb"
-  $log.debug "XXX:  DDD got #{text.size} "
+  $log.debug "DDD got #{text.size} "
   str = display_list text, :title => "Select a file"
   #$log.debug "23 We got #{str} :  #{str.class} , #{str.list[str.current_index]}  "
   $log.debug "23 We got #{str} "
@@ -54,6 +54,7 @@ def execute_this(meth, *args)
 end
 
 App.new do 
+  @startdir ||= File.expand_path("..")
   def show file
     w = @form.by_name["tv"]
     if File.directory? file
@@ -66,57 +67,78 @@ App.new do
       w.title "[ #{file} ]"
     end
   end
-  def testchoose
+  def testchoosedir
     # list filters as you type
     $log.debug "called CHOOSE " if $log.debug? 
-    filter = "*.rb"
-    #filter = ENV['PWD']+"/*"
     str = choose_file  :title => "Select a file", 
       :recursive => true,
       :dirs => true,
-      :help_text => "Enter first char/s and tab to complete filename. Scroll with C-n, C-p"
+      :directory => @startdir,
+      :help_text => "Enter pattern, use UP DOWN to traverse, Backspace to delete, ENTER to select. Esc-Esc to quit"
     if str
+      message "We got #{str} " 
+      show str
+    end
+  end
+  def testchoosefile
+    # list filters as you type a pattern
+    glob = "**/*.rb"
+    str = choose_file  glob, :title => "Select a file", 
+      :directory => @startdir,
+      :help_text => "Enter pattern, use UP DOWN to traverse, Backspace to delete, ENTER to select. Esc-Esc to quit"
+    if str and str != ""
       message "We got #{str} " 
       show str
     end
   end
   ht = 24
   borderattrib = :reverse
-  @header = app_header "canis #{Canis::VERSION}", :text_center => "Bottomline Test", 
-    :text_right =>"Click :", :color => :white, :bgcolor => 235
-  message "Press F10 to exit, F1 Help, : for Menu  "
+  @header = app_header "canis #{Canis::VERSION}", :text_center => "rCommandline Test", 
+    :text_right =>"Press :", :color => :white, :bgcolor => 236
+  message "Press F10 (or qq) to exit, F1 Help, : for Menu  "
 
 
     
     # commands that can be mapped to or executed using M-x
     # however, commands of components aren't yet accessible.
     def get_commands
-      %w{ testchoose testnumberedmenu testdisplay_list testdisplay_text testdir }
+      %w{ testchoosedir testchoosefile testnumberedmenu testdisplay_list testdisplay_text testdir }
     end
     def help_text
       <<-eos
-               BOTTOMLINE HELP 
+               rCommandLine HELP 
 
       These are some features for either getting filenames from user
-      at the bottom of the window like vim and others do.
+      at the bottom of the window like vim and others do, or filtering
+      from a list (like ControlP plugin). Or seeing a file at bottom
+      of screen for a quick preview.
 
       :        -   Command mode
       F1       -   Help
       F10      -   Quit application
+      qq       -   Quit application
 
       Some commands for using bottom of screen as vim and emacs do.
       These may be selected by pressing ':'
 
-      testchoose       - filter directory list as you type
+      testchoosedir       - filter directory list as you type
+                            '>' to step into a dir, '<' to go up.
+      testchoosefile       - filter file list as you type
+                             ENTER to select, C-c or Esc-Esc to quit
       testdir          - vim style, tabbing completes matching files
       testnumberedmenu - use menu indexes to select options
       testdisplaylist  - display a list at bottom of screen
-                         Press <ENTER> to select.
+                         Press <ENTER> to select, arrow keys to traverse, 
+                         and characters to filter list.
       testdisplaytext  - display text at bottom (current file contents)
                          Press <ENTER> when done.
 
       The file/dir selection options are very minimally functional. Improvements
       and thorough testing are required. I've only tested them out gingerly.
+
+      testchoosedir and file were earlier like Emacs/memacs with TAB completion
+      but have now moved to the much faster and friendlier ControlP plugin like
+      'filter as you type' format.
 
       -----------------------------------------------------------------------
       :n or Alt-n for general help.
@@ -126,9 +148,12 @@ App.new do
     #install_help_text help_text
 
     def app_menu
+      @curdir ||= Dir.pwd
+      Dir.chdir(@curdir) if Dir.pwd != @curdir
       require 'canis/core/util/promptmenu'
       menu = PromptMenu.new self do
-        item :c, :testchoose
+        item :c, :testchoosedir
+        item :f, :testchoosefile
         item :d, :testdir
         item :n, :testnumberedmenu
         item :l, :testdisplay_list
