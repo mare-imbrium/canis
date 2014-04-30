8f905906d6ed6cab96025badc479aeb5144368b9
kepler <githubkepler.50s@gishpuppy.com>
2014-05-01 00:00:36 +0530

    tested ButtonGroup

--- lib/canis/core/widgets/rwidget.rb
+++ lib/canis/core/widgets/rwidget.rb
@@ -9,7 +9,7 @@
   * Author: jkepler (ABCD)
   * Date: 2008-11-19 12:49 
   * License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-  * Last update: 2014-04-30 19:26
+  * Last update: 2014-04-30 23:55
 
   == CHANGES
   * 2011-10-2 Added PropertyVetoException to rollback changes to property
@@ -3131,9 +3131,94 @@ module Canis
 
   # This is not a visual class or a widget.
   # This class allows us to attach several RadioButtons to it, so it can maintain which one is the 
-  # selected one.
-  class ButtonGroup
-  end
+  # selected one. It also allows for assigning of commands to be executed whenever a button is pressed,
+  # akin to binding to the +fire+ of the button, except that one would not have to bind to each button,
+  # but only once here.
+  #
+  # @example
+  #     group = ButtonGroup.new
+  #     group.add(r1).add(r2).add(r3)
+  #     # change the color of +somelabel+ to the color specified by the value of clicked radio.
+  #     group.command(somelabel) do |grp, label| label.color(grp.value); end
+  #
+  # Added on 2014-04-30
+  class ButtonGroup # -- {{{
+
+    # Array of buttons that have been added.
+    attr_reader :elements
+
+    # the value of the radio button that is selected. To get the button itself, use +selection+.
+    attr_reader :value
+    def initialize 
+      @elements = []
+      @hash = {}
+    end
+
+    def add e
+      @elements << e
+      @hash[e.value] = e
+      e.variable(self)
+      self
+    end
+    def remove e
+      @elements.delete e
+      @hash.delete e.value
+      self
+    end
+
+    # @return the radiobutton that is selected
+    def selection
+      @hash[@value]
+    end
+
+    # @param [String, RadioButton] +value+ of a button, or +Button+ itself to check if selected.
+    # @return [true or false] for wether the given value or button is the selected one
+    def selected? val
+      if val.is_a? String
+        @value == val
+      else
+        @hash[@value] == val
+      end
+    end
+    # install trigger to call whenever a value is updated
+    # @public called by user components
+    def command *args, &block
+      @commands ||= []
+      @args ||= []
+      @commands << block
+      @args << args
+    end
+    # select the given button or value. 
+    # This may be called by user programs to programmatically select a button
+    def select button
+      if button.is_a? String
+        ;
+      else
+        button = button.value
+      end
+      set_value button
+    end
+
+    # when a radio button is pressed, it calls set_value giving the value of that radio.
+    # it also gives the name (optionally) since Variables can also be passed and be used across 
+    # groups. Here, since a button group is only for one group, so we discard name.
+    # @param [String] value (text) of radio button that is selected
+    #
+    # This is used by RadioButton class for backward compat with Variable.
+    def set_value value, name=nil
+      @value = value
+      return unless @commands
+      @commands.each_with_index do |comm, ix|
+        comm.call(self, *@args[ix]) unless comm.nil?
+      end
+    end
+    # returns the value of the selected button
+    # NOTE: This is used by RadioButton class for backward compat with Variable.
+    # User programs should use +value()+
+    def get_value name=nil
+      @value
+    end
+  end # -- }}}
   ##
   # A selectable button that has a text value. It is based on a Variable that
   # is shared by other radio buttons. Only one is selected at a time, unlike checkbox
