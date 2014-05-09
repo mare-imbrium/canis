25cfb168ed3db2345e5d779a5d3e51aa0f639edd
kepler <githubkepler.50s@gishpuppy.com>
2014-05-09 21:00:55 +0530

    initial commit

---
+++ examples/testflowlayout.rb
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------- #
+#         File: testflowlayout.rb
+#  Description: 
+#       Author: j kepler  http://github.com/mare-imbrium/canis/
+#         Date: 2014-05-09 - 20:24
+#      License: MIT
+#  Last update: 2014-05-09 20:39
+# ----------------------------------------------------------------------------- #
+#  testflowlayout.rb  Copyright (C) 2012-2014 j kepler
+
+if __FILE__ == $PROGRAM_NAME
+  require 'canis/core/util/app'
+  require 'canis/core/widgets/listbox'
+  require 'canis/core/include/layouts/flowlayout'
+  App.new do
+
+    # if i add form here, it will repaint them first, and have no dimensions
+    lb = Listbox.new @form, :list => ["borodin","berlioz","bernstein","balakirev", "elgar"] , :name => "mylist"
+    lb1 = Listbox.new @form, :list => ["bach","beethoven","mozart","gorecki", "chopin","wagner","grieg","holst"] , :name => "mylist1"
+
+    lb2 = Listbox.new @form, :list => `gem list --local`.split("\n") , :name => "mylist2"
+=begin
+
+    alist = %w[ ruby perl python java jruby macruby rubinius rails rack sinatra pylons django cakephp grails] 
+    str = "Hello, people of Earth.\nI am HAL, a textbox.\nUse arrow keys, j/k/h/l/gg/G/C-a/C-e/C-n/C-p\n"
+    str << alist.join("\n")
+    tv = TextPad.new @form, :name => "text", :text => str.split("\n")
+=end
+
+    w = Ncurses.COLS-1
+    h = Ncurses.LINES-3
+    #layout = StackLayout.new :height => -1, :top_margin => 1, :bottom_margin => 1, :left_margin => 1
+    layout = FlowLayout.new :height => -1, :top_margin => 1, :bottom_margin => 1, :left_margin => 1
+    layout.form = @form
+    @form.layout_manager = layout
+    layout.cset lb, :weight, 15
+    layout.cset lb1, :weight, 25
+    #$status_message.value =" Flow: #{@r.components[0].orientation} | Stack #{@r.components[1].orientation}. Use Ctrl-Space to change "
+
+
+    st = status_line :row => -1
+  end # app
+end # if 
