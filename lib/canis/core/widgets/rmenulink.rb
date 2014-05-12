0e14e76a8d33da01d1f6fa4528850b643d68fbda
kepler <githubkepler.50s@gishpuppy.com>
2014-05-12 23:12:07 +0530

    added KEY_ENTER in mapping for fire

--- lib/canis/core/widgets/rmenulink.rb
+++ lib/canis/core/widgets/rmenulink.rb
@@ -9,6 +9,14 @@ module Canis
       super
       @col_offset = -1 * (@col || 1)
       @row_offset = -1 * (@row || 1)
+      # in this case, we wish to use ENTER for firing
+      bind_key( KEY_ENTER, "fire" ) { fire }
+      # next did not work
+      #bind_key( KEY_ENTER, "fire" ) { get_action( 32 ) }
+      # next 2 work
+      #bind_key( KEY_ENTER, "fire" ) { @form.window.ungetch(32)  }
+      #@_key_map[KEY_ENTER] = @_key_map[32]
+      #get_action_map()[KEY_ENTER] = get_action(32)
     end
     # added for some standardization 2010-09-07 20:28 
     # alias :text :getvalue # NEXT VERSION
