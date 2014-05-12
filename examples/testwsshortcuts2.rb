7854cc83c967e56b4a51cb0f52801daa9af09e11
kepler <githubkepler.50s@gishpuppy.com>
2014-05-12 12:51:15 +0530

    use return value of confirm() so we don't think there's a bug in button

--- examples/testwsshortcuts2.rb
+++ examples/testwsshortcuts2.rb
@@ -97,7 +97,7 @@ class SetupMessagebox
       flow do
         #box do
           button :text => "  Ok  " do  alert "Pressed okay"  end
-          button :text => "Cancel" do  confirm "Do you wish to Quit?" ; throw :close;  end
+          button :text => "Cancel" do  throw :close if(confirm "Do you wish to Quit?") ; end
           button :text => "Apply "
         #end
       end
