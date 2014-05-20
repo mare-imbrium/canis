d94929148aa1fcbd75ca1299198eb01409ba6643
kepler <githubkepler.50s@gishpuppy.com>
2014-05-21 00:13:18 +0530

    New approach printing

--- lib/canis/core/util/colorparser.rb
+++ lib/canis/core/util/colorparser.rb
@@ -7,10 +7,12 @@
 #       Author: jkepler http://github.com/mare-imbrium/canis/
 #         Date: 07.11.11 - 13:17
 #      License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-#  Last update: 2013-04-01 13:42
+#  Last update: 2014-05-19 12:56
 # ----------------------------------------------------------------------------- #
 # == TODO
 #    - perhaps we can compile the regexp once and reuse
+# == CHANGES
+#    - adding style 2014-05-19 - 12:52 
 # 
 
 module Canis
@@ -40,8 +42,11 @@ module Canis
         if e[0,2] == "#[" && e[-1,1] == "]"
           # now resetting 1:20 PM November 3, 2011 , earlier we were  carrying over
           color, bgcolor, attrib = nil, nil, nil
+          style = nil
           catch(:done) do
             e = e[2..-2]
+            # TODO we could atthis point check against a hash to see if this string exists, and take
+            # the array from there and pass back so we don't keep splitting and parsing.
             ## first split on commas to separate fg, bg and attr
             atts = e.split /\s*,\s*/
               atts.each { |att|  
@@ -52,6 +57,8 @@ module Canis
                   color = part[1]
                 when "bg"
                   bgcolor = part[1]
+                when "style"
+                  style = part[1]
                 when "/end", "end"
                   yield :endcolor if block_given?
                   #next
@@ -64,7 +71,7 @@ module Canis
             # 2013-03-25 - 13:31 if numeric color specified
             color = color.to_i if color =~ /^[0-9]+$/
               bgcolor = bgcolor.to_i if bgcolor =~ /^[0-9]+$/
-              yield [color,bgcolor,attrib] if block_given?
+              yield [color,bgcolor,attrib,style] if block_given?
           end # catch
         else
           text = e
