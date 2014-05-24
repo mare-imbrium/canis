11542895e3d891a14e1ec7d9162a061c72338738
kepler <githubkepler.50s@gishpuppy.com>
2014-05-24 20:39:16 +0530

    changed logic of parser so parent colors taken at render time

--- lib/canis/core/include/colorparser.rb
+++ lib/canis/core/include/colorparser.rb
@@ -4,7 +4,7 @@
 #       Author: jkepler http://github.com/mare-imbrium/canis/
 #         Date: 07.11.11 - 12:31 
 #  Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-#  Last update: 2014-05-23 19:44
+#  Last update: 2014-05-24 20:19
 # ------------------------------------------------------------ #
 #
 
@@ -14,28 +14,62 @@ module Canis
 
     # A chunk is a piece of text with associated color and attrib.
     # Several such chunks make a ChunkLine.
+    # 2014-05-24 - 11:52 adding parent, and trying to resolve at time of render
+    #   so changes in form;s color can take effect without parsing the tree again.
+    #
+    # +color+ is a color pair which is already resolved with parent's color at time of 
+    # parsing. We need to store bgcolor and color so we can resolve at render time if nil.
+    #
     class Chunk
 
       # color_pair of associated text
       # text to print
       # attribute of associated text
       #attr_accessor :color, :text, :attrib
+      # hope no one is accessing chunk since format can change to a hash
       attr_reader :chunk
+      attr_accessor :parent
+      attr_writer :color, :bgcolor
 
-      def initialize color, text, attrib
-        @chunk = [ color, text, attrib ]
+      # earlier color was being resolved at parse time. Now with chunk change 2014-05-24 - 12:41 
+      #  color should be nil if not specified. Do not use parent's color.
+      #  Please set fgcolor and bgcolor if present, so we can resolve later.
+      def initialize color_pair, text, attrib
+        @chunk = [ color_pair, text, attrib ]
         #@color = color
         #@text  = text
         #@attrib = attrib
       end
-      def color
-        @chunk[0]
+      #
+      # This is to be called at runtime by render_all or render to resolve
+      # the color.
+      # @return [color_pair, nil] color pair for chunk, if nil then substitute with 
+      #   the default which will be form's color. If the form has set fg and bg, then nil
+      #   should not be returned ever.
+      def color_pair
+        #@chunk[0] 
+        # if the color was set, use return it.
+        return @chunk[0] if @chunk[0]
+        if @color && @bgcolor
+          # color was not set, but fg and bg both were
+          @chunk[0] = get_color(nil, @color, @bgcolor)
+          return @chunk[0] if @chunk[0]
+        end
+        # return a resolved color_pair if we can, but do not store it in tree.
+        # This will be resolved each time render is called from parent.
+        return get_color(nil, self.color(), self.bgcolor())
       end
       def text
         @chunk[1]
       end
       def attrib
-        @chunk[2]
+        @chunk[2] || @parent.attrib
+      end
+      def color
+        @color || @parent.color
+      end
+      def bgcolor
+        @bgcolor || @parent.bgcolor
       end
     end
 
@@ -66,7 +100,7 @@ module Canis
         @chunks.each do |chunk| 
           case chunk
           when Chunks::Chunk
-            color = chunk.color
+            color = chunk.color_pair
             attrib = chunk.attrib
             text = chunk.text
           when Array
@@ -136,6 +170,15 @@ module Canis
         @bgcolor_array = [@bgcolor]
         @attrib_array = [@attrib]
         @color_pair_array = [@color_pair]
+        @parents = nil
+      end
+      # this is the widget actually that created the parser
+      def form=(f)
+        # FIXME this means if textpad color changes then i won't be getting it !!!
+        #ch = Chunk.new nil, nil, f.attr
+        #ch.color = f.color
+        #ch.bgcolor = f.bgcolor
+        @parents = [f]
       end
 
       # since 2014-05-19 - 13:14 
@@ -156,6 +199,9 @@ module Canis
       # @yield chunk if block
       # @return [ChunkLine] # [Array] array of chunks
       # @since 1.4.1   2011-11-3 experimental, can change
+      # 2014-05-24 - 12:54 NEW As of now since this is called at parse time
+      #   colors must not be hardcoded, unless both fg and bg are given for a chunk.
+      #   The color_pair should be resolved at render time using parent.
       public
       def convert_to_chunk s, colorp=$datacolor, att=FFI::NCurses::A_NORMAL
 
@@ -169,9 +215,11 @@ module Canis
         bgcolor = @bgcolor
         # stack the values, so when user issues "/end" we can pop earlier ones
 
+        newblockflag = false
         @color_parser.parse_format(s) do |p|
           case p
           when Array
+            newblockflag = true
             ## got color / attrib info, this starts a new span
 
             # added style 2014-05-19 - 12:57 maybe should be a hash
@@ -185,9 +233,18 @@ module Canis
               lb ||= map[:bgcolor]
               la ||= map[:attrib]
             end
+            @_bgcolor = lb
+            @_color = lc
             if la
               @attrib = get_attrib la
             end
+            @_color_pair = nil
+            if lc && lb
+              # we know only store color_pair if both are mentioned in style or tag
+              @_color_pair = get_color(nil, lc, lb)
+            end
+
+=begin
             if lc || lb
               @color = lc ? lc : @color_array.last
               @bgcolor = lb ? lb : @bgcolor_array.last
@@ -195,14 +252,22 @@ module Canis
               @bgcolor_array << @bgcolor
               @color_pair = get_color($datacolor, @color, @bgcolor)
             end
+=end
             @color_pair_array << @color_pair
             @attrib_array << @attrib
-            $log.debug "XXX: CHUNK start cp=#{@color_pair} , a=#{@attrib} :: c:#{lc} b:#{lb} : @c:#{@color} @bg: #{@bgcolor} "
-            $log.debug "XXX: CHUNK start arr #{@color_pair_array} :: #{@attrib_array} ::#{@color_array} ::: #{@bgcolor_array} "
+            #$log.debug "XXX: CHUNK start cp=#{@color_pair} , a=#{@attrib} :: c:#{lc} b:#{lb} : @c:#{@color} @bg: #{@bgcolor} "
+            #$log.debug "XXX: CHUNK start arr #{@color_pair_array} :: #{@attrib_array} ::#{@color_array} ::: #{@bgcolor_array} "
 
           when :endcolor
 
             # end the current (last) span
+            @parents.pop unless @parents.count == 1
+            @_bgcolor = @_color = nil
+            @_color_pair = nil
+
+            $log.debug "XXX: CHUNK end parents:#{@parents.count}, last: #{@parents.last} "
+            #$log.debug "XXX: CHUNK end arr #{@color_pair_array} :: #{@attrib_array} "
+            #
             @color_pair_array.pop
             @color_pair = @color_pair_array.last
             @attrib_array.pop
@@ -210,8 +275,8 @@ module Canis
             # why are we nt popping the color and bgcolor array dts
             @color_array.pop unless @color_array.count == 1
             @bgcolor_array.pop unless @bgcolor_array.count == 1
-            $log.debug "XXX: CHUNK end #{color_pair} , #{attrib} "
-            $log.debug "XXX: CHUNK end arr #{@color_pair_array} :: #{@attrib_array} "
+            #$log.debug "XXX: CHUNK end #{color_pair} , #{attrib} "
+            #$log.debug "XXX: CHUNK end arr #{@color_pair_array} :: #{@attrib_array} "
           when :reset   # ansi has this
             # end all previous colors
             @color_pair = $datacolor # @color_pair_array.first
@@ -224,10 +289,19 @@ module Canis
           when String
 
             ## create the chunk
-            $log.debug "XXX:  CHUNK     using on #{p}  : #{@color_pair} , #{@attrib} " # 2011-12-10 12:38:51
+            #$log.debug "XXX:  CHUNK     using on #{p}  : #{@color_pair} , #{@attrib} " # 2011-12-10 12:38:51
+            $log.debug "XXX:  CHUNK     using on #{p}  : #{@_color_pair} , #{@attrib}, fg: #{@_color}, #{@_bgcolor}, parent: #{@parents.last} " # 2011-12-10 12:38:51
 
             #chunk =  [color_pair, p, attrib] 
-            chunk = Chunk.new @color_pair, p, @attrib
+            chunk = Chunk.new @_color_pair, p, @attrib
+            chunk.color = @_color
+            chunk.bgcolor = @_bgcolor
+            chunk.parent = @parents.last
+            if newblockflag
+              @parents << chunk
+              $log.debug "XXX: CHUNK start parents:#{@parents.count}, #{@parents.last} "
+              newblockflag = true
+            end
             if block_given?
               yield chunk
             else
