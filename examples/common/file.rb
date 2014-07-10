ca719e54363f6c64414b1f3ba908667a40da705b
kepler <githubkepler.50s@gishpuppy.com>
2014-07-11 01:21:17 +0530

    added file_listing

--- examples/common/file.rb
+++ examples/common/file.rb
@@ -5,7 +5,7 @@
 #       Author: jkepler http://github.com/mare-imbrium/canis/
 #         Date: 2011-11-15 - 19:54
 #      License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-#  Last update: 2014-06-30 21:40
+#  Last update: 2014-07-10 14:49
 # ----------------------------------------------------------------------------- #
 # NOTE after calling shell_out you now need to call Window.refresh_all if you have
 # pads on the screen which are getting left black
@@ -44,6 +44,49 @@ module Canis
       alert "#{fp} is not text, not paging "
       #use_on_file "als", fp # only zip or archive
     end
+  end
+  def file_listing path, config={}
+    listing = config[:mode] || :SHORT
+    ret = []
+    if File.exists? path
+      files = Dir.new(path).entries
+      files.delete(".")
+      return files if listing == :SHORT
+
+      files.each do |f| 
+        if listing == :LONG
+          pf = File.join(path, f)
+          if File.exists? pf
+            $log.info "  File (#{f}) found "
+          else
+            $log.info "  File (#{f}) NOT found "
+          end
+          stat = File.stat(pf)
+          ff = "%10s  %s  %s" % [readable_file_size(stat.size,1), date_format(stat.mtime), f]
+          ret << ff
+        end
+      end
+    end
+    return ret
+  end
+  ## code related to long listing of files
+  GIGA_SIZE = 1073741824.0
+  MEGA_SIZE = 1048576.0
+  KILO_SIZE = 1024.0
+
+  # Return the file size with a readable style.
+  def readable_file_size(size, precision)
+    case
+      #when size == 1 : "1 B"
+    when size < KILO_SIZE then "%d B" % size
+    when size < MEGA_SIZE then "%.#{precision}f K" % (size / KILO_SIZE)
+    when size < GIGA_SIZE then "%.#{precision}f M" % (size / MEGA_SIZE)
+    else "%.#{precision}f G" % (size / GIGA_SIZE)
+    end
+  end
+  ## format date for file given stat
+  def date_format t
+    t.strftime "%Y/%m/%d"
   end
 
 end # module
