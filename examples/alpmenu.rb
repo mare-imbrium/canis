4eef4b214bbaf6fc8f81eb8a73312302726141c6
ChrisArcand <chris@chrisarcand.com>
2014-09-22 08:46:09 -0400

    Fix pathing in alpmenu example

--- examples/alpmenu.rb
+++ examples/alpmenu.rb
@@ -1,6 +1,10 @@
 require 'canis/core/util/app'
 
-  App.new do 
+  def load_relative(path)
+    load File.expand_path("../#{path}", __FILE__)
+  end
+
+  App.new do
     #title "Demo of Menu - canis"
     #subtitle "Hit F1 to quit, F2 for menubar toggle"
     header = app_header "canis #{Canis::VERSION}", :text_center => "Alpine Menu Demo", :text_right =>""
@@ -10,24 +14,24 @@ require 'canis/core/util/app'
       w = 60
       menulink :text => "&View Dirs", :width => w, :description => "View Dirs in tree"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './dirtree.rb'
+        load_relative './dirtree.rb'
         #require './viewtodo'; todo = ViewTodo::TodoApp.new; todo.run
       end
       blank
       menulink :text => "&Tabular", :width => w, :description => "Tabula Rasa"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './tabular.rb'
+        load_relative './tabular.rb'
         #require './testtodo'; todo = TestTodo::TodoApp.new; todo.run
       end
       blank
       menulink :text => "&Messages", :width => w, :description => "View Tasks"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './tasks.rb'
+        load_relative './tasks.rb'
       end
       blank
       menulink :text => "&Database", :width => w, :description => "Database Demo"  do |s, *stuff|
         message "Pressed #{s.getvalue} "
-        load './dbdemo.rb'
+        load_relative './dbdemo.rb'
       end
       blank
       # somehow ? in mnemonic won't work
